03f75ecde403c40b761ad02081fb07360fbdaadf
GustaJLemos <gustavolemosmendes@gmail.com>
2022-11-10 22:20:14 -0300

    Criando screen com o estyled components

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Title } from './styles'
+
+export function Groups() {
+  return (
+    <Container>
+      <Title>
+        Opa tudo bem?
+      </Title>
+    </Container>
+  );
+}
