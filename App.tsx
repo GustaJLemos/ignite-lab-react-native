63d39b94ef9a0d2622c1b39ea3f1602e809e8558
GustaJLemos <gustavolemosmendes@gmail.com>
2022-11-13 00:04:13 -0300

    Adicionando statusBar e fontes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components'
 import theme from '@theme/index'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { ActivityIndicator, StatusBar } from 'react-native';
 
 export default function App() {
+  const [fonstLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      {fonstLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
