c09bb0728c69337445aeded98ebcdf747d42e2a4
GustaJLemos <gustavolemosmendes@gmail.com>
2022-11-13 22:08:35 -0300

    Usando os componentes

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,41 @@
+import { Button } from '@components/Button';
+import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
 import React from 'react';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 import { Container } from './styles'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocketseat', 'Amigos da facul']);
+
   return (
     <Container>
       <Header showBackButton={false}/>
+
+      <Highlight title='Turmas' subtitle='Jogue com a sua turma'/>
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+            onPress={() => console.log('cu')}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message='Que tal cadastrar a primeira turma?'
+          />
+        )}
+      />
+
+      <Button title='Criar nova turma'/>
     </Container>
   );
 }
