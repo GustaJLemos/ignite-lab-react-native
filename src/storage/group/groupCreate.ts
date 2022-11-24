357c9497ed54746adb533fe8d1a4b9a9aa7620e9
GustaJLemos <gustavolemosmendes@gmail.com>
2022-11-23 22:50:01 -0300

    criando lógicas da store

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,10 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLLECTION } from "@storage/storageConfig";
+import { AppError } from "@utils/AppError";
 import { groupsGetAll } from "./groupsGetAll";
 
 export async function groupCreate(newGroup: string) {
   try {
     const storedGroups = await groupsGetAll();
+
+    const groupAlreadyExists = storedGroups.includes(newGroup)
+
+    if(groupAlreadyExists) {
+      throw new AppError('Já existe um grupo cadastrado com esse nome.');
+    }
 
     // preciso salvar esse cara aq como uma string
     const storage = JSON.stringify([...storedGroups, newGroup])
