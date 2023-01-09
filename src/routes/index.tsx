98308f0878618eedefe2e9c6cab60bd9317de692
GustaJLemos <gustavolemosmendes@gmail.com>
2023-01-08 22:11:11 -0300

    Criando as configs das rotas

---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { Box, useTheme } from 'native-base';
+import { AuthRoutes } from './auth.routes';
+
+export function Routes() {
+  const nativeBaseTheme = useTheme();
+
+  const theme = DefaultTheme;
+  theme.colors.background = nativeBaseTheme.colors.gray[700];
+
+  return (
+    // esse box a gente deixa, pra nossa rota sempre ocupar tudo, e em transições, caso de algum glitch ele não apareça uma tela em branco
+    <Box flex={1} bg='gray.700'>
+      {/* conseguimos editar o tema (por ex cor do background) através desse cara aqui */}
+      <NavigationContainer theme={theme}>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
+  );
+}
