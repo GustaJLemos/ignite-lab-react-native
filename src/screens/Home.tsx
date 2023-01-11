b8ec968144a71f7e65e9c1c45a6aa673aa998c83
GustaJLemos <gustavolemosmendes@gmail.com>
2023-01-11 20:57:10 -0300

    Terminando de construir a interface

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,27 +1,56 @@
+import { ExerciseCard } from "@components/ExerciseCard";
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { HStack, VStack } from "native-base";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
 import { useState } from "react";
 
 export function Home() {
+  const [groups, setGroups] = useState(['costas', 'ombro', 'biceps', 'triceps'])
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada', 'Remada lateral', 'triceps'])
   const [groupSelected, setGroupSelected] = useState('costas');
 
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <HStack>
-        <Group 
-          name='ombros' 
-          isActive={groupSelected === 'costas'}
-          onPress={() => setGroupSelected('costas')}
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        renderItem={({ item }) => (
+          <Group 
+            name={item} 
+            isActive={groupSelected.toUpperCase() === item.toUpperCase()}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent='space-between' mb={5}>
+          <Heading color='gray.200' fontSize='md'>
+            Exercícios
+          </Heading>
+
+          <Text color='gray.200' fontSize='sm'>
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList 
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 20 }}
         />
-        <Group 
-          name='costas' 
-          isActive={groupSelected === 'ombro'}
-          onPress={() => setGroupSelected('ombro')}
-        />
-      </HStack>
+      </VStack>
     </VStack>
   );
 }
