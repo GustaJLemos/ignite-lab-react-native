7b65ba58e537b98826276ad265ef47d61cc54249
GustaJLemos <gustavolemosmendes@gmail.com>
2023-01-16 20:01:50 -0300

    Adicionando estilzações

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -3,9 +3,9 @@ import { Heading, HStack, Text, VStack } from "native-base";
 export function HistoryCard() {
   return (
     <HStack w='full' bg='gray.600' rounded='md' alignItems='center' justifyContent='space-between' px={5} py={4} mb={3}>
-      <VStack mr={5}>
+      <VStack mr={5} flex={1}>
         {/* capitalize mande a primeira letra maiúscula */}
-        <Heading color='white' fontSize='sm' textTransform='capitalize'>
+        <Heading color='white' fontSize='sm' textTransform='capitalize' numberOfLines={1}>
           Costas
         </Heading>
         <Text color='gray.100' fontSize='lg' numberOfLines={1}>
