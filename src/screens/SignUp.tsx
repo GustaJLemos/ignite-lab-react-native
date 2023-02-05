694743ebd3d845386a321f9ebd8a8ea645ba524d
GustaJLemos <gustavolemosmendes@gmail.com>
2023-02-05 18:08:54 -0300

    Consumindo o back end da aplicação

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base';
 
 import BackgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
@@ -8,7 +8,10 @@ import { useNavigation } from '@react-navigation/native'
 import { useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookForm/resolvers/yup';
-
+import { api } from '@services/api';
+import axios from 'axios';
+import { Alert } from 'react-native';
+import { AppError } from '@utils/AppError';
 
 type FormDataProps = {
   name: string;
@@ -32,6 +35,8 @@ const signUpSchema = yup.object({
 })
 
 export function SignUp() {
+  const toast = useToast();
+
   const { control, handleSubmit, formState: { errors }, reset } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema)
   });
@@ -50,10 +55,56 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp(data: FormDataProps) {
-    console.log(data);
-    // resetando o valor do form
-    reset(DEFAULT_VALUES)
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    // podemos usar o fetch para nos comunicarmos com a api
+    // const response = await fetch('http://192.168.15.1:3333/users', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     name, email, password
+    //   })
+    // })
+
+    // com o axios n precisamos tranformar a resposta, ou seja, a resposta ela já vem convertida em json,
+    // const data = await response.json();
+
+    // podemos pegar erros, e respontas através do axios de duas formas
+    // api.post('/users', {
+    //   name, email, password
+    // }).then((res) => console.log(res))
+    // .catch((err) => console.log(err))
+
+
+    try {
+      const response = await api.post('/users', {
+        name, email, password
+      })
+  
+      console.log(response.data)
+  
+      // resetando o valor do form
+      reset(DEFAULT_VALUES)
+    } catch(err) {
+      // aq estamos verificando, se é um erro que veio do back end, que foi pego pelo axios, se for um erro 
+      // que veio por uma lógica errada do try por ex, ele não entra nesse if
+      // if(axios.isAxiosError(err)) {
+      //   if(err.response?.data?.message) {
+      //     Alert.alert('Estamos com problemas...', `${err.response?.data?.message}`)
+      //   }
+      // }
+
+      const isAppError = err instanceof AppError;
+      const title = isAppError ? err.message : 'Não foi possível criar a conta. Tente novamente mais tarde!'
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    }
   }
 
   return (
