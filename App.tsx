0edec62209eb35b0d251cb7f5b21c55bdb5653d7
GustaJLemos <gustavolemosmendes@gmail.com>
2023-02-07 22:50:15 -0300

    Consumindo a context

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { Loading } from '@components/Loading';
 import { SignIn } from '@screens/SignIn';
 import { Routes } from '@routes/index';
 import { NavigationContainer } from '@react-navigation/native';
+import { AuthContext, AuthContextProvider } from '@contexts/AuthContext';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +18,9 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      { fontsLoaded ? <Routes /> : <Loading />  }
+      <AuthContextProvider>
+        { fontsLoaded ? <Routes /> : <Loading />  }
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
