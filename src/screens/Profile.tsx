9db011e278f0d509b375d7db99383f8429b12384
GustaJLemos <gustavolemosmendes@gmail.com>
2023-02-25 16:11:41 -0300

    Fazendo validações e função para atualizar os dados

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -8,7 +8,11 @@ import { Alert, TouchableOpacity } from "react-native";
 import { Controller, useForm } from 'react-hook-form';
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useAuth } from "@hooks/useAuth";
+import { AppError } from "@utils/AppError";
+import { api } from "@services/api";
 
 const PHOTO_SIZE = 33
 
@@ -20,19 +24,40 @@ type FormDataProps = {
   confirm_password: string;
 }
 
+const profileSchema = yup.object({
+  name: yup.string().required('Informe o nome.'),
+  // a gente faz esse nullable pq? pq como falamos q a senha deve ter no mínimo 6, ele n vai passar no form
+  // e se eu n quiser atualizar minha senha? isso n passa na validação, então a gente deixa isso, falando tipol
+  // esse campo pode ser nulo, se ele n tiver valor, fale q é nulo (ai n cai nessa validação de 6 digitos), e se tiver valor
+  // ele é o próprio value dai.
+  password: yup.string().min(6, 'A senha deve ter pelo menos 6 dígitos.')
+    .nullable().transform((value) => !!value ? value : null),
+  confirm_password: yup.string().nullable().transform((value) => !!value ? value : null)
+    .oneOf([yup.ref('password'), null], 'A confirmação de senha não confere.')
+    .when('password', {
+      // aq estamos fazendo basicamente, se existir valor no password
+      // a gente executa o then e esse campo fica required
+      is: (Field: any) => Field,
+      then: yup.string().nullable().required('Informe a confirmação da senha.')
+        .transform((value) => !!value ? value : null)
+    }),
+})
+
 export function Profile() {
+  const [isUpdating, setIsUpdating] = useState(false);
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState('https://github.com/gustajlemos.png');
 
-  const { user } = useAuth();
+  const { user, updateUserProfile } = useAuth();
 
   const toast = useToast();
 
-  const { control } = useForm<FormDataProps>({
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     defaultValues: {
       name: user.nome,
       email: user.email,
-    }
+    },
+    resolver: yupResolver(profileSchema)
   });
 
   async function handleUserPhotoSelect() {
@@ -77,6 +102,36 @@ export function Profile() {
     }
   }
 
+  async function handleProfileUpdate(data: FormDataProps) {
+    try {
+      setIsUpdating(true);
+
+      const userUpdated = user;
+      userUpdated.nome = data.name;
+
+      await api.put('/users', data);
+
+      await updateUserProfile(userUpdated);
+
+      toast.show({
+        title: 'Perfil atualizado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500'
+      });
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível atualizar os dados.';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsUpdating(false);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil"/>
@@ -113,6 +168,7 @@ export function Profile() {
                 value={value}
                 onChangeText={onChange}
                 placeholder="Nome"
+                errorMessage={errors.name?.message}
                 bg='gray.600'
               />
             )}
@@ -123,39 +179,64 @@ export function Profile() {
             name='email'
             render={({ field: {value, onChange} }) => (
               <Input 
-                value={value}
                 placeholder='E-mail'
-                onChangeText={onChange}
                 isDisabled
+                value={value}
+                onChangeText={onChange}
                 bg='gray.600'
               />
             )}
           />
 
-          
-        
           <Heading color='gray.200' fontSize='md' fontFamily='heading' mb={2} mt={12} alignSelf='flex-start'>
             Alterar senha
           </Heading>
 
-          <Input 
-            placeholder="Senha antiga"
-            secureTextEntry
-            bg='gray.600'
-          />
-          <Input 
-            placeholder="Nova senha"
-            secureTextEntry
-            bg='gray.600'
-          />
-          <Input 
-            placeholder="Confirme a nova senha"
-            secureTextEntry
-            bg='gray.600'
+          <Controller 
+            control={control}
+            name='old_password'
+            render={({ field: {value, onChange} }) => (
+              <Input 
+                placeholder="Senha antiga"
+                secureTextEntry
+                onChangeText={onChange}
+                bg='gray.600'
+              />
+            )}
           />
 
+          <Controller 
+            control={control}
+            name='password'
+            render={({ field: {value, onChange} }) => (
+              <Input 
+                placeholder="Nova senha"
+                secureTextEntry
+                onChangeText={onChange}
+                errorMessage={errors.password?.message}
+                bg='gray.600'
+              />
+            )}
+          />
+          
+          <Controller 
+            control={control}
+            name='confirm_password'
+            render={({ field: {value, onChange} }) => (
+              <Input 
+                placeholder="Confirme a nova senha"
+                secureTextEntry
+                onChangeText={onChange}
+                errorMessage={errors.confirm_password?.message}
+                bg='gray.600'
+              />
+            )}
+          />
+          
           <Button 
             title='Atualizar'
+            onPress={handleSubmit(handleProfileUpdate)}
+            isLoading={isUpdating}
             mt={4}
           />
         </Center>
