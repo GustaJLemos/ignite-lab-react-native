fc06546c13be79f6ad7a5a3a339273860fede1a3
GustaJLemos <gustavolemosmendes@gmail.com>
2023-05-25 22:23:31 -0300

    Setando a chave do oneSignal

--- App.tsx
+++ App.tsx
@@ -9,6 +9,11 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
+import OneSignal from 'react-native-onesignal';
+import Constants from "expo-constants";
+
+OneSignal.setAppId('1b9bd6fc-d99b-4ea6-a098-98289e2480a6');
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
