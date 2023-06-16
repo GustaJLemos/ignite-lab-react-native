36a9799990e25cd0c4e51a5b6823a6fcc7d9edda
GustaJLemos <gustavolemosmendes@gmail.com>
2023-06-16 00:19:52 -0300

    Criando função para retornar o dia da semana

--- src/utils/Date.ts
+++ src/utils/Date.ts
@@ -1,9 +1,14 @@
-import { format, formatDistanceToNowStrict } from "date-fns"
+import { format, formatDistanceToNow } from "date-fns"
+import { ptBR } from 'date-fns/locale'
 
 export function getCurrentDate() {
   return format(new Date(), 'dd-MM-yyyy');
 }
 
 export function getDistanceOfDates(date: string) {
-  return formatDistanceToNowStrict(new Date(date), { addSuffix: false, unit: 'day' });
+  return formatDistanceToNow(new Date(date), { addSuffix: true, locale: ptBR });
+}
+
+export function returnDayOfWeek() {
+  return format(new Date(), 'eeee');
 }
